7a8bc13ab98d575952e717504eb10654154f4e28
Taqueer Ali Tapya <tauqeertapya123@gmail.com>
2024-11-24 01:03:00 +0530

    index.js and package.json modified

--- index.js
+++ index.js
@@ -8,26 +8,25 @@ import morgan from 'morgan'
 import { fileURLToPath } from 'url';
 import productRouter from './routes/product.js'
 
-// main().catch(err => console.log(err))
+main().catch(err => console.log(err))
 
-// async function main() {
-//     await mongoose.connect(process.env.MONGO_URL)
-//     console.log('Database Connected')
-// }
+async function main() {
+    await mongoose.connect(process.env.MONGO_URL)
+    console.log('Database Connected')
+}
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-console.log(__dirname);
 
-// const app = express()
+const app = express()
 
-// app.use(cors())
-// app.use(express.static(path.join(__dirname, process.env.PUBLIC_DIR)))
-// app.use(express.json())
-// app.use(morgan('tiny'))
-// app.use('/products', productRouter)
-// app.use('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
-// })
+app.use(cors())
+app.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)))
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use('/products', productRouter)
+app.use('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+})
 
-// app.listen(process.env.PORT)
+app.listen(process.env.PORT)

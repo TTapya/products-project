303af0a35cca91da62fcab855023f08974e5bd2f
Taqueer Ali Tapya <tauqeertapya123@gmail.com>
2024-11-24 01:18:02 +0530

    index.js modified

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const __dirname = path.dirname(__filename);
 const app = express()
 
 app.use(cors())
-app.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)))
+app.use(express.static(path.resolve(__dirname, 'dist')))
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use('/products', productRouter)
